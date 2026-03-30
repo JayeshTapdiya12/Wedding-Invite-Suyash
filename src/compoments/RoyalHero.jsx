ac18c0494fbd9ba36d208e081bf7db90b6290ede
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2026-03-30 21:47:37 +0530

    intro mei video link add kiya hai

--- src/compoments/RoyalHero.jsx
+++ src/compoments/RoyalHero.jsx
@@ -6,7 +6,7 @@ const RoyalHero = () => {
         <section className="hero-section">
             <div className="video-container">
                 <video autoPlay playsInline className="hero-video">
-                    <source src="/assests/introvideo.mp4#t=0,5.2" type="video/mp4" />
+                    <source src="https://res.cloudinary.com/dwsv6ggaa/video/upload/v1774887364/introvideo_puwjw2.mp4#t=0,5.2" type="video/mp4" />
                 </video>
             </div>
         </section>
