bdf66fd24dc7ec37e98358ab4c2300eb25b1c13f
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2026-04-11 02:24:54 +0530

    blessing cred

--- src/compoments/Blessing.jsx
+++ src/compoments/Blessing.jsx
@@ -11,7 +11,7 @@ const Blessings = () => {
     const sendEmail = (e) => {
         e.preventDefault();
         emailjs
-            .sendForm("service_dhqe5b4", "service_kxet42e", form.current, "irbr7SFLiqDuVojKa")
+            .sendForm("service_kxet42e", "template_kojq5ca", form.current, "irbr7SFLiqDuVojKa")
             .then(() => {
                 setSubmitted(true);
                 setShowHearts(true);
